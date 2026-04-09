007e3b5ced7d7c6a3daadba3ed33a0b06ed39035
Daniel Lyytikäinen <daniel.lyytikainen@yh.nackademin.se>
2026-04-09 11:52:24 +0200

    first commit

---
+++ js/order.js
@@ -0,0 +1,84 @@
+const product = JSON.parse(localStorage.getItem("selectedProduct"));
+const productDiv = document.getElementById("selected-product");
+const form = document.getElementById("orderForm");
+const message = document.getElementById("message");
+const cancelBtn = document.getElementById("cancel-button");
+const phoneInput = document.getElementById("phone");
+const zipInput = document.getElementById("zip");
+
+if (phoneInput) {
+  phoneInput.addEventListener("input", function () {
+    this.value = this.value.replace(/[^0-9\-()]/g, "");
+  });
+}
+if (zipInput) {
+  zipInput.addEventListener("input", function () {
+    this.value = this.value.replace(/[^0-9-()]/g, "");
+  });
+}
+
+if (productDiv) {
+  if (product) {
+    productDiv.innerHTML = `<h3>${product.title}</h3>`;
+  } else {
+    productDiv.innerHTML = `<p>No product selected.</p>`;
+  }
+}
+
+if (cancelBtn) {
+  cancelBtn.addEventListener("click", function () {
+    const confirmCancel = confirm(
+      "Are you sure you want to cancel this order?",
+    );
+
+    if (confirmCancel) {
+      localStorage.removeItem("selectedProduct");
+      window.location.href = "index.html";
+    }
+  });
+}
+
+if (form) {
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const phone = document.getElementById("phone").value.trim();
+    const street = document.getElementById("street").value.trim();
+    const zip = document.getElementById("zip").value.trim();
+    const city = document.getElementById("city").value.trim();
+
+    let error = "";
+
+    if (name.length < 2 || name.length > 50) {
+      error = "Name must be between 2 and 50 characters";
+    } else if (!email.includes("@") || email.length > 50) {
+      error = "Email must contain @ and be max 50 characters";
+    } else if (!/^[0-9\-()]+$/.test(phone) || phone.length > 20) {
+      error =
+        "Phone number can only contain numbers, hyphens, and parentheses, max 20 characters";
+    } else if (street.length < 2 || street.length > 50) {
+      error = "Street address must be between 2 and 50 characters";
+    } else if (!/^[0-9]{5}$/.test(zip)) {
+      error = "Postal code must be exactly 5 digits";
+    } else if (city.length < 2 || city.length > 20) {
+      error = "City must be between 2 and 20 characters";
+    }
+
+    if (error) {
+      message.style.color = "red";
+      message.innerText = error;
+      return;
+    }
+
+    message.style.color = "green";
+
+    form.reset();
+    localStorage.removeItem("selectedProduct");
+
+    setTimeout(() => {
+      window.location.href = "thankyou.html";
+    }, 500);
+  });
+}
---
+++ js/products.js
@@ -0,0 +1,45 @@
+﻿fetch("https://fakestoreapi.com/products")
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("FakeStore is down");
+    }
+    return res.json();
+  })
+  .catch(async () => {
+    const res = await fetch("https://dummyjson.com/products/?limit=18");
+    return await res.json();
+  })
+  .then((data) => {
+    const products = Array.isArray(data) ? data : data.products;
+
+    const container = document.getElementById("products");
+    container.innerHTML = "";
+
+    products.forEach((product) => {
+      const image = product.image || product.thumbnail;
+      const title = product.title;
+      const price = product.price;
+
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      card.innerHTML = `
+        <img src="${image}" width="100">
+        <h3>${title}</h3>
+        <p class="price">${price} $</p>
+        <button>Buy Now</button>
+      `;
+
+      const button = card.querySelector("button");
+
+      button.addEventListener("click", () => {
+        localStorage.setItem("selectedProduct", JSON.stringify(product));
+        window.location.href = "order.html";
+      });
+
+      container.appendChild(card);
+    });
+  })
+  .catch((err) => {
+    console.error("Both APIs failed:", err);
+  });
